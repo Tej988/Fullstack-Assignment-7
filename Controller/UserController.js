09fe9583966ec1e15e18921a9c9457e4955c8ef5
Tejprakash <67041377+Tej988@users.noreply.github.com>
2021-06-19 14:18:56 +0530

    update

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const User = require("../Models/UserModel");
 const mongoose = require("mongoose");
-const Blog = require('../Models/BlogModel');
+
 
 
 
@@ -63,37 +63,3 @@ exports.getUserById = (req, res) => {
         });
 };
 
-exports.postBlog = (req, res) => {
-    let { heading, blog, userId } = req.body;
-    let blogC = new Blog({
-        heading,
-        blog,
-        userId,
-    });
-    blogC
-        .save()
-        .then(() => res.status(200).send(blogC))
-        .catch(() => {
-            console.error(error);
-            return res.status(404).send("Error")
-        })
-}
-
-exports.getBlog = (req,res)=>{
-    let id = req.params.id;
-    id = mongoose.Types.ObjectId(id);
-    Blog.findOne({ _id:id})
-    .then((blogC) => {
-        if (blogC) {
-            console.info("Blog found");
-            return res.status(200).send(blogC);
-        }
-        console.error("Blog was not found");
-        return res.status(404).send("Not found ");
-    })
-    .catch((error) => {
-        console.error(eror);
-        return res.status(500).send("error")
-
-    });
-}
